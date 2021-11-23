c392dd0d2abf4b975b82ee975b417033a793ddde
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2021-11-23 20:09:56 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Do you want source code?
-✯ Source: <a href=https://github.com/Akbotmakersnew/EvaMaria>Click Here</a>
+✯ Source: <a href=https://github.com/Akbotmakersnew/ManualAutofilterbot>Click Here</a>
 
 <b>DEVS:</b>
 - <a href=https://t.me/Ak_Bot_SupportGroup>Team Ak-BotDevelopers</a>"""
@@ -21,7 +21,7 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. MANUAL AUTO FILTER BOT should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Ak- MANUAL AUTO FILTER BOT both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. AK-MANUAL AUTO FILTER BOT supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/carnivalcinemas007)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of MANUAL AUTO FILTER BOT
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
